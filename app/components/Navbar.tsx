60978f7eedbf2f5001135de8e4b72481e5a9b17f
Atharva <sinhaatharva688@gmail.com>
2025-08-21 19:16:15 +0530

    Resume feedback components

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router";
+
+const Navbar: () => React.JSX.Element = ()=>{
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+            </Link>
+            <Link to="/upload" className="primary-button w-fit">
+                Upload Resume
+                </Link>
+        </nav>
+    )
+}
+export default Navbar;
